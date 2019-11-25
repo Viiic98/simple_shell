77b6018d7cc86e39ac47946b5116a7526b98528c
oran2527 <orlago250183@gmail.com>
2019-11-25 14:12:30 +0000

    add strncmp function

---
+++ strncmp.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strncmp - Function that compares c chars between two strings 
+ *
+ * @s1: First string that will be compared
+ * @s2: Second string that will be compared
+ * @c: Amount 
+ * 
+ * Return: Integer depends that the ASCII table
+ */
+int _strncmp(char *s1, char *s2, int c)
+{
+	int i, a;
+
+	a = 0;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0' || i < c; i++)
+	{
+		a = a + (s1[i] - s2[i]);
+		if (a != 0)
+		{
+			break;
+		}
+	}
+
+	return (a);
+}
