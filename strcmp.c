4c872606db60cfc0c3b435a8bc0ddaa741e5c764
Victor Arteaga <xathovic@gmail.com>
2019-11-25 15:31:25 +0000

    Remove extra lines

--- strcmp.c
+++ strcmp.c
@@ -9,19 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, a;
-
-	a = 0;
+	int i, a = 0;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		a = a + (s1[i] - s2[i]);
 		if (a != 0)
-		{
 			break;
-		}
 	}
-
 	return (a);
 }
 /**
@@ -35,18 +30,13 @@ int _strcmp(char *s1, char *s2)
  */
 int _strncmp(char *s1, char *s2, int c)
 {
-	int i, a;
-
-	a = 0;
+	int i, a = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && i < c; i++)
 	{
 		a = a + (s1[i] - s2[i]);
 		if (a != 0)
-		{
 			break;
-		}
 	}
-
 	return (a);
 }
