1e447e109d214d3398ad76d2636b96267c624df7
oran2527 <orlago250183@gmail.com>
2019-11-26 22:05:23 +0000

    modifying conditionals and limits

---
+++ atoi.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ * _atoi - function to convert char to int
+ * @i: length of second argument
+ * @buf: chars second argument
+ **/
+int _atoi(int i, char* buf)
+{
+	char letters[10] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };   
+	int numbers[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int j, k, res = 0, num, flag, n = 1, p, exp;
+
+	for (j = 0; buf[j] != '\0'; j++)
+	{
+		flag = 0;
+		exp = 1;
+		for (k = 0; k < 10; k++)
+		{
+			if (buf[j] == letters[k])
+			{
+				num = numbers[k];
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 1)
+		{			
+			for (p = i - n; p > 0; p--)
+				exp *= 10;
+			if (i - n > 0)
+				res += num * exp;
+			if (i - n == 0)
+				res += num;
+			n++;
+		}
+		if (flag == 0)
+			exit(0);
+	}
+	if (res >= 256 && res < 2147483647)
+		return (res - 256);
+	if (res < 256)
+		return (res);
+}
